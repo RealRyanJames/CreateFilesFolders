33a9fbd3ae363e09bda71654fe4e129897a1dbc8
RealRyanJames <realcoder2023@gmail.com>
2026-07-17 20:31:56 -0400

    init javascript maps file

--- src/main.js
+++ src/main.js
@@ -1,8 +1,32 @@
-import { fs1 } from "../imports/imports.js";
-import { App, FileNameCreation, isEmpty, OnErrorFile, } from "../Functions/Functions.js";
+import { isEmpty, OnErrorFile } from "../Functions/Functions.js";
+import {} from "../imports/imports.js";
+class onGetUser {
+    getArgs(getFileName, name) {
+        return getFileName(name);
+    }
+}
+const onArgsRead = (index) => {
+    const readArgs = process.argv.slice(index);
+    return readArgs;
+};
+const upperFileName = (file) => {
+    return file.toUpperCase();
+};
 const main = () => {
-    const q = getOnStrings();
-    OnErrorFile(q);
+    const argsUser = onArgsRead(2);
+    const argsUserIndex = onArgsRead(3);
+    const upperTextMessage = new onGetUser();
+    if (String(argsUser) == "-cr" && String(argsUserIndex) == "-f") {
+        const q = getOnStrings();
+        upperTextMessage.getArgs(upperFileName, q.question);
+        OnErrorFile(q);
+    }
+    else {
+        const qIndex = getOnStrings();
+        upperTextMessage.getArgs(upperFileName, qIndex.question);
+        const q = getOnStrings();
+        OnErrorFile(q);
+    }
 };
 const getOnStrings = () => {
     const q = {
