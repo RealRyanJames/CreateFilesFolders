ca390feddc9bfcfde50acb0e002fa1156865b251
RealRyanJames <realcoder2023@gmail.com>
2026-07-17 20:14:15 -0400

    added class and check if some args equals to -cr and 3rd -f

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,38 @@
 import { isEmpty, OnErrorFile } from "../Functions/Functions.js";
 
-import { fs1, type FILENAME, type UserQuestion } from "../imports/imports.js";
+import { type FILENAME, type UserQuestion } from "../imports/imports.js";
+
+class onGetUser {
+  public getArgs(getFileName: (name: FILENAME) => string, name: string) {
+    return getFileName(name);
+  }
+}
+
+const onArgsRead = (index: number) => {
+  const readArgs = process.argv.slice(index);
+  return readArgs;
+};
+
+const upperFileName = (file: FILENAME) => {
+  return file.toUpperCase();
+};
 
 const main = () => {
-  const q = getOnStrings();
-  OnErrorFile(q);
+  const argsUser = onArgsRead(2);
+  const argsUserIndex = onArgsRead(3);
+  const upperTextMessage = new onGetUser();
+
+  if (String(argsUser) == "-cr" && String(argsUserIndex) == "-f") {
+    const q = getOnStrings();
+    upperTextMessage.getArgs(upperFileName, q.question);
+
+    OnErrorFile(q);
+  } else {
+    const qIndex = getOnStrings();
+    upperTextMessage.getArgs(upperFileName, qIndex.question);
+    const q = getOnStrings();
+    OnErrorFile(q);
+  }
 };
 
 const getOnStrings = () => {
